b078d93ea0ea61582ffb12c53c8f923bb3e3ee8f
Joyce Rigelo <jrigelo@utexas.edu>
2016-01-29 00:54:23 -0600

    exercise 28

---
+++ hard_way28.py
@@ -0,0 +1,43 @@
+# exercise 28
+# Boolean Practice
+
+print True and True
+# 1.True
+print False and True
+# 2.False
+print 1 == 1 and 2 == 1
+# 3.False
+print "test" == "test"
+# 4.True
+print 1 == 1 or 2 != 1
+# 5.True
+print True and 1 == 1
+# 6.True
+print False and 0 != 0
+# 7.False
+print True or 1 == 1
+# 8. True
+print "test" == "testing"
+# 9. False
+print 1 != 0 and 2 == 1
+# 10. False
+print "test" != "testing"
+# 11. True
+print "test" == 1
+# 12. False
+print not (True and False)
+# 13. True
+print not (1 == 1 and 0 != 1)
+# 14. False
+print not (10 == 1 or 1000 == 1000)
+# 15. False
+print not (1 != 10 or 3 == 4)
+# 16. False
+print not ("testing" == "testing" and "Zed" == "Cool Guy")
+# 17. True
+print 1 == 1 and (not ("testing" == 1 or 1 ==0))
+# 18. True
+print "chunky" == "bacon" and (not(3 ==4 or 3 ==3))
+# 19. False
+print 3 ==3 and (not("testing" == "testing" or "Python" == "Fun"))
+# 20. False
