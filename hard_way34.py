f5e11e1218687b019eb8fa7eb6a554034117ce47
Joyce Rigelo <jrigelo@utexas.edu>
2016-01-31 21:37:03 -0600

    Accessing Elements of Lists

---
+++ hard_way34.py
@@ -0,0 +1,13 @@
+# exercise 34
+# Accessing Elements of Lists
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "The animal at %d is a %s" % (1, animals[1])
+print "The (3rd) animal is at %d and it is a %s" % (2, animals[2])
+print "The (1st) animal is at %d and it is a %s" % (0, animals[0])
+print "The animal at %d is a %s" % (3, animals[3])
+print "The (5th) animal is at %d and it is a %s" % (4, animals[4])
+print "The animal at %d is a %s" % (2, animals[2])
+print "The sixth (6th) animal is at %d and it is a %s" % (5, animals[5])
+print "The animal at %d is a %s" % (4, animals[4])
